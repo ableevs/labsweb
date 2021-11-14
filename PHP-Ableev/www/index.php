087e9a8a900ee774954df3e686aec079e818653c
ableevs <77155552+ableevs@users.noreply.github.com>
2021-11-14 19:40:21 +0300

    LR1

---
+++ PHP-Ableev/www/index.php
@@ -0,0 +1,13 @@
+<p> Дата и время:
+<p>
+<?php
+ $d=date("d.m.Y H:i");
+ echo($d);
+?>
+<ul>
+	<li><a href="http://php-ableev/WWW/lab-1-1.php">lab-1-1</li>
+	<li><a href="http://php-ableev/WWW/lab-1-2.php">lab-1-2</li>
+	<li><a href="http://php-ableev/WWW/lab-1-3.php">lab-1-3</li>
+	<li><a href="http://php-ableev/WWW/lab-1-5.php">lab-1-5</li>
+	<li><a href="http://php-ableev/WWW/lab-1-5.1.php">lab-1-5.1</li>
+</ul>
---
+++ PHP-Ableev/www/lab-1-3.php
@@ -0,0 +1,7 @@
+<p> Число е равно:
+<p>
+<?php
+$NUM_E = 2.71828;
+settype($NUM_E, boolean);
+print('chislo NUM_E = '.$NUM_E. ' type = ' .gettype($NUM_E) )
+?>
